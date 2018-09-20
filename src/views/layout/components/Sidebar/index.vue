88d26a6a05af8eae9eb77e43dbda9bf59b7bab78
minru-li <minruli96@163.com>
2018-09-20 16:01:08 +0800

    add

---
+++ src/views/layout/components/Sidebar/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <el-scrollbar wrap-class="scrollbar-wrapper">
+    <el-menu
+      :show-timeout="200"
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      mode="vertical"
+      background-color="#304156"
+      text-color="#bfcbd9"
+      active-text-color="#409EFF"
+    >
+      <sidebar-item v-for="route in routes" :key="route.name" :item="route" :base-path="route.path"/>
+    </el-menu>
+  </el-scrollbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SidebarItem from './SidebarItem'
+
+export default {
+  components: { SidebarItem },
+  computed: {
+    ...mapGetters([
+      'sidebar'
+    ]),
+    // 返回所有的路由 this.$router.options.routes
+    routes() {
+      return this.$router.options.routes
+    },
+    // 判断侧边栏是否折叠
+    isCollapse() {
+      return !this.sidebar.opened
+    }
+  }
+}
+</script>
